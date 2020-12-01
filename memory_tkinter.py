82df9e9975de04228d670935d4634fe692ac1ed2
cica-mica <73489192+cica-mica@users.noreply.github.com>
2020-12-01 12:49:47 +0100

    Add files via upload

--- memory_tkinter.py
+++ memory_tkinter.py
@@ -10,17 +10,17 @@ root.title('Memory Game')
 root.resizable(False, False)
 
 # These are eight images that I used for cards in the game, there are two cards with the same image on it
-image1 = ImageTk.PhotoImage(Image.open('C:/Users/Korisnik/Documents/uvod_u_programiranje/predavanje-04/images/—Pngtree—beautiful red poinsettia flower christmas_5566767.png'))
-image2 = ImageTk.PhotoImage(Image.open('C:/Users/Korisnik/Documents/uvod_u_programiranje/predavanje-04/images/—Pngtree—happy thanksgiving with pumpkin corn_5512176.png'))
-image3 = ImageTk.PhotoImage(Image.open('C:/Users/Korisnik/Documents/uvod_u_programiranje/predavanje-04/images/butterfly.jpg'))
-image4 = ImageTk.PhotoImage(Image.open('C:/Users/Korisnik/Documents/uvod_u_programiranje/predavanje-04/images/—Pngtree—cartoon squirrel_1474628.png'))
-image5 = ImageTk.PhotoImage(Image.open('C:/Users/Korisnik/Documents/uvod_u_programiranje/predavanje-04/images/—Pngtree—cute yellow flat islamic mosque_5512716.png'))
-image6 = ImageTk.PhotoImage(Image.open('C:/Users/Korisnik/Documents/uvod_u_programiranje/predavanje-04/images/39091.jpg'))
-image7 = ImageTk.PhotoImage(Image.open('C:/Users/Korisnik/Documents/uvod_u_programiranje/predavanje-04/images/—Pngtree—red vintage camera vector clipart_4236390.png'))
-image8 = ImageTk.PhotoImage(Image.open('C:/Users/Korisnik/Documents/uvod_u_programiranje/predavanje-04/images/—Pngtree—winter beautiful symmetrical dark blue_5527848.png'))
+image1 = ImageTk.PhotoImage(Image.open('./images/—Pngtree—beautiful red poinsettia flower christmas_5566767.png'))
+image2 = ImageTk.PhotoImage(Image.open('./images/—Pngtree—happy thanksgiving with pumpkin corn_5512176.png'))
+image3 = ImageTk.PhotoImage(Image.open('./images/butterfly.jpg'))
+image4 = ImageTk.PhotoImage(Image.open('./images/—Pngtree—cartoon squirrel_1474628.png'))
+image5 = ImageTk.PhotoImage(Image.open('./images/—Pngtree—cute yellow flat islamic mosque_5512716.png'))
+image6 = ImageTk.PhotoImage(Image.open('./images/39091.jpg'))
+image7 = ImageTk.PhotoImage(Image.open('./images/—Pngtree—red vintage camera vector clipart_4236390.png'))
+image8 = ImageTk.PhotoImage(Image.open('./images/—Pngtree—winter beautiful symmetrical dark blue_5527848.png'))
 
 # Background is the image on the wrongside of every card
-background = ImageTk.PhotoImage(Image.open('C:/Users/Korisnik/Documents/uvod_u_programiranje/predavanje-04/predavanje_9/snake.png'))
+background = ImageTk.PhotoImage(Image.open('./predavanje_9/snake.png'))
 
 # The list of images (16 elements), there are two elements of every image, which are afterwards shuffled
 images_list = [image1, image2, image3, image4, image5, image6, image7, image8, image1, image2, image3, image4, image5, image6, image7, image8]
@@ -36,6 +36,17 @@ game_over = tk.Label(root, text = ' ', font = ('Helvetica', '20','bold'), fg = '
 
 score = tk.Label(root, text = 'Matched pairs: ' + str(len(disabled_buttons)//2), font = ('Helvetica', '14','bold'), fg = '#163A1F')
 
+def restart():
+    global counter, picks_list, disabled_buttons, images_list
+    #restart_game.config(state = 'enabled')
+    counter = 0
+    picks_list = []
+    random.shuffle(images_list)
+    for c in disabled_buttons:
+        c.config(image = background)
+        c.config(state = 'active')
+    disabled_buttons = []
+
 # This is the command that allows cards to turn over, turn back, freeze when the images are the same
 def folded_card(button, number):
     global images_list
@@ -44,45 +55,37 @@ def folded_card(button, number):
     global disabled_buttons
     global game_over
     global score
-    
-    game_over.grid_forget()
-    game_over = tk.Label(root, text = ' ', font = ('Helvetica', '20','bold'), fg = '#163A1F')
-    game_over.grid(row = 4, column = 0, pady = 20, columnspan = 4)
+    global restart_game
     
     # first condition is if the counter is less than two because a player is allowed to turn over only two cards at once
     if counter < 2:
-
-        game_over.grid_forget()
-        game_over = tk.Label(root, text = ' ', font = ('Helvetica', '20','bold'), fg = '#163A1F')
-        game_over.grid(row = 4, column = 0, pady = 20, columnspan = 4)
 
         button['image'] = images_list[number]
         picks_list.append(button)
         counter +=1
     
     # when counter = 2 is the perfect time to check if the cards match
-    elif counter == 2:
-        game_over.grid_forget()
+    else:
+        
         if picks_list[0] == picks_list[1]:
             counter -=1
             picks_list.remove(picks_list[1])
-            game_over = tk.Label(root, text = 'Oooops!', font = ('Helvetica', '20','bold'), fg = '#163A1F')
-            game_over.grid(row = 4, column = 0, pady = 20, columnspan = 4)
+            game_over.config(text = 'Oooops')
+            
         else:
             if picks_list[0]['image'] == picks_list[1]['image']:
                 for b in picks_list:
                     b['state'] = 'disabled'
                     disabled_buttons.append(b)
-
-                    score = tk.Label(root, text = 'Matched pairs: ' + str(len(disabled_buttons)//2), font = ('Helvetica', '14','bold'), fg = '#163A1F')
-                    score.grid(row = 5, column = 2, pady = 15, columnspan = 2)
+                    score.config(text = 'Matched pairs: ' + str(len(disabled_buttons)//2))
 
                     if len(disabled_buttons) == 16:
-                        game_over = tk.Label(root, text = 'GAME OVER!', font = ('Helvetica', '20', 'bold'), fg = '#163A1F')
-                        game_over.grid(row = 4, column = 0, pady = 20, columnspan = 4)
+                        game_over.config(text = 'GAME OVER!')
+                        restart_game
+                        
                     else:
-                        game_over = tk.Label(root, text = 'MATCH!', font = ('Helvetica', '20','bold'), fg = '#163A1F')
-                        game_over.grid(row = 4, column = 0, pady = 20, columnspan = 4)
+                        game_over.config(text = 'MATCH!')
+                       
                 counter = 0
                 picks_list = []
             else:
@@ -90,18 +93,10 @@ def folded_card(button, number):
                     b['image'] = background
                 counter = 0
                 picks_list = []
-                game_over = tk.Label(root, text = 'Wrong Match! Try Again.', font = ('Helvetica', '20','bold'), fg = '#163A1F')
-                game_over.grid(row = 4, column = 0, pady = 20, columnspan = 4)
-    else:
-        for b in picks_list:
-           b['image'] = background
-        counter = 0
-        picks_list = []       
+                game_over.config(text = 'Wrong Match! Try again.')
+                
 
-# quit command exits the window
-def quit():
-    root.destroy()
-    
+
 # there are 16 cards turned upside-down
 card1 = tk.Button(root, bg = '#AEE7BA',image = background, command = lambda:folded_card(card1, 0))
 card2 = tk.Button(root, bg = '#AEE7BA',image = background, command = lambda:folded_card(card2, 1))
@@ -121,7 +116,8 @@ card15 = tk.Button(root, bg = '#AEE7BA',image = background, command = lambda:fol
 card16 = tk.Button(root, bg = '#AEE7BA',image = background, command = lambda:folded_card(card16, 15))
 
 # This is the button that, besides X, allows you to quit the game
-exit_game = tk.Button(root, text = 'EXIT', font = ('Helmetica', '16', 'bold'), fg = '#163A1F', bg = '#9FAFA3', command = quit)
+exit_game = tk.Button(root, text = 'EXIT', font = ('Helmetica', '16', 'bold'), fg = '#163A1F', bg = '#9FAFA3', command = root.destroy)
+restart_game = tk.Button(root, text = 'RESTART', font = ('Helvetica', '16', 'bold'), fg = '#163A1F', bg = '#9FAFA3', relief = 'ridge', command = restart)
 
 card1.grid(row = 0, column = 0)
 card2.grid(row = 0, column = 1)
@@ -145,5 +141,7 @@ game_over.grid(row = 4, column = 0, padx = 8, pady = 20, columnspan = 4)
 score.grid(row = 5, column = 2, pady = 15, columnspan = 2)
 
 exit_game.grid(row = 5, column = 0, columnspan = 2, padx = 8, pady = 13, sticky = tk.W)
+
+restart_game.grid(row = 6, column = 0, pady = 10, padx = 8)
 
 root.mainloop()
